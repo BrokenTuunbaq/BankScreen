7f13a0f5f3abd310d3ffb902e9f7c2f3a0629316
BrokenTuunbaq <BrokenTuunbaq>
2020-01-14 12:09:28 +0200

    Comment added

--- BankScreen/ResultsData.swift
+++ BankScreen/ResultsData.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-//Class for testing without network request
+//Class for testing without network request (offline)
 struct ResultsData {
     static let results: String = "{\"Meta Data\": {\"1. Information\": \"Intraday (1min) open, high, low, close prices and volume\",\"2. Symbol\": \"JPM\",\"3. Last Refreshed\": \"2020-01-10 16:00:00\",\"4. Interval\": \"1min\",\"5. Output Size\": \"Compact\",\"6. Time Zone\": \"US/Eastern\"},\"Time Series (1min)\": {\"2020-01-10 16:00:00\": {\"1. open\": \"136.0400\",\"2. high\": \"136.1000\",\"3. low\": \"136.0400\",\"4. close\": \"136.0900\",\"5. volume\": \"112981\"},\"2020-01-10 15:59:00\": {\"1. open\": \"136.0000\",\"2. high\": \"136.0700\",\"3. low\": \"135.9700\",\"4. close\": \"136.0382\",\"5. volume\": \"82884\"}}}"
 }
